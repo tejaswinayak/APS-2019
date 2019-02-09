aae0e72119afb32631a4bb0d0b740f895095f819
tejaswinayak <34745631+tejaswinayak@users.noreply.github.com>
2019-02-09 19:45:09 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int fibo(int n)
+{
+    int fib[n+1];
+    int i;
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (i = 2; i < n + 1; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    return fib[n];
+}
+
+
+int main()
+{
+    int n;
+    int result;
+    printf("Enter the required nth number in fibonacci series: ");
+    scanf("%d", &n);
+
+    if (n == 0)
+        result = 0;
+    else
+        result = fibo(n);
+    printf("The %d th number in fibonacci series is %d\n", n, result);
+
+    return 0;
+}
